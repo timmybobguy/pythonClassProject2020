09ae78705745fc67c74b5ac875c31c975b57f8a5
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2020-03-16 00:18:48 +1300

    Started adding CMD and commands to project

--- run_cmd.py
+++ run_cmd.py
@@ -4,28 +4,51 @@ import os
 import cmd
 import ast
 import pylint
+import graphviz
+import subprocess
+
 
 class CLI(cmd.Cmd):
 
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = "===>>>>>> "
-        self.intro = "This programme can generate class diagrams from your source codes"
+        self.intro = "This program can generate class diagrams from your source codes, type help for a list of commands"
 
     def do_hello(self, arg):
-        print( "hello again", arg, "!")
+        """Greets user"""
+        print("hello again", arg, "!")
+
+    def do_printFile(self, path):
+        """Takes one parameter: file path. Takes a file and then prints it to the console"""
+        try:
+
+            fileObj = open(path, 'r')
+            print(fileObj.read())
+
+        except FileNotFoundError:
+
+            print("No file found at: '" + path + "'")
+
+    def do_generateDOT(self, args):
+        """Takes two parameters: source code file path, and output (dot) file path. This command turns the source
+        code file into a dot file of the program structure"""
+
+        arguments = args.split()
+
+        if len(arguments) == 2:
+
+            print("correct number")
+            print(arguments[0])
+            print(arguments[1])
+
+            command = 'pyreverse dot ' + arguments[0] + ' -p componentplain -o ' + arguments[1]
+            subprocess.call(command)
+
+        else:
+
+            print("Wrong num of args, please try again")
 
 
-    def do_run(self, arg):
-    # get the file name/path
-       fileObj = open('./test1.py', 'r')
-       print(fileObj.read())
-
-
-
-
-
-
-
-cli = CLI()
-cli.cmdloop()
+if __name__ == '__main__':
+    CLI().cmdloop()
--- test1.py
+++ test1.py
@@ -4,22 +4,29 @@
 class Equus:
     def __init__(self, is_male):
         pass
+
     def run(self):
         print(f'I run at speed {self.speed}')
         print(f'I\'m {self.gender}')
+
+
 class Horse(Equus):
     def __init__(self, is_male):
         print('Horse init')
         self.speed = Donkey(False)
         self.gender = 'male' if is_male else 'female'
         self.is_horse = True
+
     def roar(self):
         print('Hee haw~')
+
+
 class Donkey(Equus):
     def __init__(self, is_female):
         print('Donkey init')
         self.speed = 20
         self.gender = 'female' if is_female else 'male'
         self.is_donkey = True
+
     def roar(self):
         print('Hee haw hee hee haw~')
