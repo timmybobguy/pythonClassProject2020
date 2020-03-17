6412030b87cf45a8240cb25799d121328a7c2d4d
JimmyC.WU <jimmy07096@gmail.com>
2020-03-18 12:10:09 +1300

    file directory

--- validate_data.py
+++ validate_data.py
@@ -13,13 +13,13 @@ class ValidateData():
 
     def check_file(self, input_file):
         get_file_name = os.path.dirname(input_file)
-        command = "pycodestyle --statistics -qq " + input_file
+        command = "pycodestyle " + input_file
         print(command)
         subprocess.run(command, cwd=get_file_name, shell=True)
 
 
 if __name__ == '__main__':
-    ValidateData().check_file("/Users/jimmy/py/pythonClassProject2020/test2.py")
+    ValidateData().check_file("/Users/jimmy/py/pythonClassProject2020/run_cmd.py")
 
 
 
