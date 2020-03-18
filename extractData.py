3afe1d16c6d7cab43f25b8c4c0a218410af72da0
JimmyC.WU <jimmy07096@gmail.com>
2020-03-19 10:35:56 +1300

    extract data- get a table

--- extractData.py
+++ extractData.py
@@ -1,6 +1,8 @@
 #!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 # -*- coding: utf-8 -*-
 import re
+import os
+import plotly.graph_objects as go
 
 
 class ExtractData:
@@ -8,17 +10,22 @@ class ExtractData:
         pass
 
     def Get_file(self, cmd_file):
+        filename = os.path.basename(cmd_file)
         file = open(cmd_file)
         file1 = file.read()
         obj = re.findall(r"def\sdo\w+", file1, re.S)
+        all = []
         for i in obj:
             j = i.strip('def')
             print(j)
-
+            all.append(j)
+        fig = go.Figure(data=[go.Table(header=dict(values=[filename]), cells=dict(values=[all]))])
+        fig.show()
 
 
 if __name__ == '__main__':
     ExtractData().Get_file("run_cmd.py")
+
 
 
 
