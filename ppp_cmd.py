03b4ef5365863b9355b66d100fe2bb6afa8e8161
JimmyC.WU <jimmy07096@gmail.com>
2020-03-22 01:32:53 +1300

    add exceptions

--- ppp_cmd.py
+++ ppp_cmd.py
@@ -43,27 +43,35 @@ class CLI(cmd.Cmd):
         raw_data = args.split()
         input_file = raw_data[0]
         file_type = raw_data[1]
-        os.path.isfile(input_file)
-        work_dir = os.path.dirname(input_file)
-        file = input_file[len(work_dir) + 1:]
-        if file_type == 'svg':
-            command = "pyreverse {0} -o svg -p diagram".format(file)
-            print("input_dir:{0} input_file:{1}".format(work_dir, file))
-            print("command:" + command)
-            subprocess.run(command, cwd=work_dir, shell=True)
-        elif file_type == 'fig':
-            command = "pyreverse {0} -o fig -p diagram".format(file)
-            print("input_dir:{0} input_file:{1}".format(work_dir, file))
-            print("command:" + command)
-            subprocess.run(command, cwd=work_dir, shell=True)
-        elif file_type == 'dot':
-            command = "pyreverse {0} -o dot -p diagram".format(file)
-            print("input_dir:{0} input_file:{1}".format(work_dir, file))
-            print("command:" + command)
-            subprocess.run(command, cwd=work_dir, shell=True)
+        try:
+                os.path.isdir(input_file)
+                os.path.isfile(input_file)
+                os.path.exists(filepath)
+                work_dir = os.path.dirname(input_file)
+                file = input_file[len(work_dir) + 1:]
+        except:
+                print("wrong path, try again")
 
         else:
-            print(" You only have 3 options which are svg, dot and fig in the file types")
+
+            if file_type == 'svg':
+                command = "pyreverse {0} -o svg -p diagram".format(file)
+                print("input_dir:{0} input_file:{1}".format(work_dir, file))
+                print("command:" + command)
+                subprocess.run(command, cwd=work_dir, shell=True)
+            elif file_type == 'fig':
+                command = "pyreverse {0} -o fig -p diagram".format(file)
+                print("input_dir:{0} input_file:{1}".format(work_dir, file))
+                print("command:" + command)
+                subprocess.run(command, cwd=work_dir, shell=True)
+            elif file_type == 'dot':
+                command = "pyreverse {0} -o dot -p diagram".format(file)
+                print("input_dir:{0} input_file:{1}".format(work_dir, file))
+                print("command:" + command)
+                subprocess.run(command, cwd=work_dir, shell=True)
+
+            else:
+                print(" You only have 3 options which are svg, dot and fig in the file types")
 
     def do_extractData(self, args):
         """This can load a cmd interface file and extract data from it"""
@@ -72,7 +80,10 @@ class CLI(cmd.Cmd):
 
     def do_ValidateData(self,args):
         "input a full path of your file, then the programme can check your file weather pass pep8 or not"
-        ValidateData().check_file(args)
+        try:
+            ValidateData().check_file(args)
+        except:
+            print("wrong path, try again")
 
     def do_bar_chart(self, args):
         """input a full path of your file, then the programme can generate a bar chart
@@ -82,17 +93,27 @@ class CLI(cmd.Cmd):
 
     def do_mySql(self, arg):
         """this can check weather the db links """
-        link_db().check_link_db()
+        try:
+            link_db().check_link_db()
+        except:
+            print("wrong path, try again")
 
     def do_save_data(self, args):
         """this can save data to MySql database"""
-        link_db().get_file(args)
-        link_db().link_mysql_save()
+        try:
+            link_db().get_file(args)
+            link_db().link_mysql_save()
+        except:
+            print("wrong path, try again")
 
 
     def do_load_data(self, arg):
          """this can laod data from MySql database"""
-         link_db().load_mysql_data()
+         try:
+             link_db().load_mysql_data()
+         except:
+             print("wrong path, try again")
+
 
 
 if __name__ == '__main__':
