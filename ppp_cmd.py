01d4c9e3555e4d1b6ed86053565608ef0f993875
tib0259 <tib0259@L130-60822>
2020-03-23 13:32:12 +1300

    updating function names

--- ppp_cmd.py
+++ ppp_cmd.py
@@ -57,7 +57,7 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
             print('Help data file cannot be opened...')
         self.__json = json
 
-    def do_getAllSourceFiles(self, arg):
+    def do_get_all_source_files(self, arg):
         arguments = arg.split()
 
         if len(arguments) != 0:
@@ -80,7 +80,7 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
             else:
                 print("Directory not valid")
 
-    def help_getAllSourceFiles(self):
+    def help_get_all_source_files(self):
         print(self.__json.get_help_text('getAllSourceFiles'))
 
     def do_shelve(self, cmd_file):
@@ -146,24 +146,24 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
     def help_uml_diagram(self):
         print(self.__json.get_help_text('uml_diagram'))
 
-    def do_extractData(self, args):
+    def do_extract_data(self, args):
         """This can load a cmd interface file and extract data from it"""
 
         ExtractData().get_data(args)
 
-    def help_extractData(self):
+    def help_extract_data(self):
         print(self.__json.get_help_text('extractData'))
 
-    def do_ValidateData(self, args):
+    def do_validate_data(self, args):
         try:
             path = True
             ValidateData().check_file(args)
-        except:
-            print("wrong path, try again")
+        except Exception as err:
+            print(err)
             path = False
         return path
 
-    def help_ValidateData(self):
+    def help_validate_data(self):
         print(self.__json.get_help_text('ValidateData'))
 
     def do_bar_chart(self, args):
@@ -173,15 +173,15 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
             path = True
             ExtractData().get_data(args)
             ExtractData().draw_bar_chart()
-        except:
-            print("wrong path, try again")
+        except Exception as err:
+            print(err)
             path = False
         return path
 
     def help_bar_chart(self):
         print(self.__json.get_help_text('bar_chart'))
 
-    def do_mySql(self, arg):
+    def do_mysql(self, arg):
         """this can check weather the db links.
         please make sure your Mysql have a database which is
         host="127.0.0.1", user="root", passwd="1234", database="class """
@@ -212,7 +212,7 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
         except FileNotFoundError:
             print("cannot load data from the database")
 
-    def do_printFile(self, path):
+    def do_print_file(self, path):
         """Takes one parameter: file path. Takes a file and then prints it to the console"""
         try:
 
@@ -223,7 +223,7 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
 
             print("No file found at: '" + path + "'")
 
-    def do_generateDOT(self, args):
+    def do_generate_dot(self, args):
         """Takes one parameter: source code file path. This command turns the source
         code file into a dot file of the program structure"""
 
@@ -249,14 +249,14 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
 
             print("Wrong num of args, please try again")
 
-    def do_saveFiletoLocalDatabase(self, arg):
+    def do_save_file_to_local_database(self, arg):
         """Saves a file to the database server"""
-        saveFileToLocalDatabase(arg)
+        save_file_to_local_database(arg)
 
-    def do_getFileFromLocalDatabase(self, arg):
-        getFileFromLocalDatabase(arg)
+    def do_get_file_from_local_database(self, arg):
+        get_file_from_local_database(arg)
 
-    async def do_testSaveToDatabaseServer(self, args):
+    async def do_test_save_to_database_server(self):
         loop = asyncio.get_event_loop()
 
         result = loop.run_until_complete(saveServer())
@@ -264,7 +264,7 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
 
         loop.close()
 
-    def do_generatePieChart(self, file):
+    def do_generate_pie_chart(self, file):
 
         try:
             with open(file, 'r') as file:
@@ -307,22 +307,22 @@ class CLI(cmd.Cmd):  # MyAsyncShell - This is not working bugged !!!
                 pie = CreatePieChart(labels, sizes)
                 pie.generate_pie_chart()
 
-    def help_generatePieChart(self):
+    def help_generate_pie_chart(self):
         print(self.__json.get_help_text('generatePieChart'))
 
 
-async def saveServer():
-    await saveFileToDatabaseServer("Components.txt")
+async def save_server():
+    await save_file_to_database_server("Components.txt")
 
 
-def convertToBinaryData(filename):
+def convert_to_binary_data(filename):
     # Convert digital data to binary format
     with open(filename, 'rb') as file:
         blobData = file.read()
     return blobData
 
 
-def saveFileToLocalDatabase(path):
+def save_file_to_local_database(path):
     conn = MySqlite()
     conn.create_connection(r"pythonsqlite.db")
     conn.create_cursor()
@@ -344,7 +344,7 @@ def saveFileToLocalDatabase(path):
     conn.close_connection()
 
 
-def getFileFromLocalDatabase(filename):
+def get_file_from_local_database(filename):
     conn = MySqlite()
     try:
         conn.create_connection(r"pythonsqlite.db")
@@ -356,7 +356,7 @@ def getFileFromLocalDatabase(filename):
     print(result)
 
 
-async def saveFileToDatabaseServer(path):
+async def save_file_to_database_server(path):
     conn = MySQL()
     db_config = {
         'host': 'localhost',
